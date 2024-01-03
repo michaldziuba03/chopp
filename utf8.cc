b76440a5a30014c3bfdb8c0179741faf4248726a
Michał Dziuba <mail@michaldziuba.dev>
2024-01-03 02:08:06 +0100

    add temporary utils for utf-8 chars

---
+++ utf8.cc
@@ -0,0 +1,70 @@
+#include "utf8.hh"
+#include <iostream>
+#include <exception>
+
+unsigned short codepointSize(char byte) {
+    if ((byte & 0x80) == 0) // 0xxxxxxx
+        return 1;
+    else if ((byte & 0xE0) == 0xC0) // 110xxxxx
+        return 2;
+    else if ((byte & 0xF0) == 0xE0) // 1110xxxx
+        return 3;
+    else if ((byte & 0xF8) == 0xF0) // 11110xxx
+        return 4;
+
+    return -1;
+}
+
+size_t columnLen(const std::string& str) {
+    size_t len = 0;
+    size_t i = 0;
+
+    while(i < str.length()) {
+        i += codepointSize(str[i]);
+        len++;
+    }
+
+    return len;
+}
+
+void removeChar(std::string& str, int pos)
+{
+    size_t i = 0; // index relative to string bytes
+    size_t j = 0; // index relative to "printable" unicode length
+
+    while(i < str.length()) {
+        size_t cSize = codepointSize(str[i]);
+        if (cSize == -1) {
+            throw std::runtime_error("Invalid UTF-8 character");
+        }
+
+        if (j == pos) {
+            str.erase(i, cSize);
+            break;
+        }
+
+        i += cSize;
+        j++;
+    }
+}
+
+// temporary shitty implementation
+void appendChar(std::string& str, int pos, const char* chars) {
+    size_t i = 0; // index relative to string bytes
+    size_t j = 0; // index relative to "printable" unicode length
+
+    while(i < str.length()) {
+        size_t cSize = codepointSize(str[i]);
+        if (cSize == -1) {
+            throw std::runtime_error("Invalid UTF-8 character");
+        }
+
+        if (j == pos) {
+            str.insert(i, chars, codepointSize(chars[0]));
+            break;
+        }
+
+        i += cSize;
+        j++;
+    }
+}
---
+++ utf8.hh
@@ -0,0 +1,5 @@
+#include <string>
+
+size_t columnLen(const std::string&);
+void removeChar(std::string&, int);
+void appendChar(std::string&, int, const char*);
